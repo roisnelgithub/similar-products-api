19721621b53976c10e6afdbd0532a210f3e0c3d7
Roy <roy.adan.trenal@gmail.com>
2025-12-02 12:32:05 -0500

    refactor: add constructor , getters and setter to Product class

--- src/main/java/com/roy/similar_products/domain/models/Product.java
+++ src/main/java/com/roy/similar_products/domain/models/Product.java
@@ -5,4 +5,44 @@ public class Product {
   String name;
   Double price;
   String availability;
+
+  public Product(String id, String name, Double price, String availability) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.availability = availability;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public String getAvailability() {
+    return availability;
+  }
+
+  public void setAvailability(String availability) {
+    this.availability = availability;
+  }
+
 }
